33bddee4a79d176b5f6c611cbd61f9af71a5d07a
kristianwahlroos <kristian.whl@gmail.com>
2020-02-25 11:49:48 +0200

    Test tracing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use tracing::{span, Level, event, info};
+use tracing_subscriber::FmtSubscriber;
+
+fn hello_world() {
+    info!("hello world");
+}
+
+fn event_span() {
+    event!(Level::INFO, "something happened");
+
+    let span = span!(Level::INFO, "my_span");
+    let _guard = span.enter();
+
+    event!(Level::DEBUG, "something happened inside my_span");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let my_subscriber = FmtSubscriber::new();
+    tracing::subscriber::set_global_default(my_subscriber).expect("settin tracing default failed");
+    hello_world();
+    event_span();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_hello() {
+        let my_subscriber = FmtSubscriber::new();
+        tracing::subscriber::set_global_default(my_subscriber).expect("settin tracing default failed");
+        hello_world();
+        event_span();
+    }
 }
